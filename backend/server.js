998a0459131ab0183f6c51573b2217ea9e2663b4
pablomarcel <pablomarcel@outlook.com>
2022-08-28 20:18:38 -0700

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,16 @@ const cors = require("cors")
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const path = require('path');
 
 connectDB();
 
 const server = express();
+
 // server.get("/") //delete if it does not work
 server.use(express.json());
-server.use(cors({origin: true}));
+// server.use(cors({origin: true}));
+server.use(cors());
 // const corsOptions = {
 //     origin: "http://localhost:3000"
 // }
@@ -24,6 +27,10 @@ server.use(cors({origin: true}));
 server.use(express.urlencoded({ extended: true }));
 server.use(errorHandler);
 server.use(routes);
+server.use('/', express.static(path.join(__dirname, '/frontend/build')))
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 server.use('/images', express.static('images'));
 
 module.exports = server;
