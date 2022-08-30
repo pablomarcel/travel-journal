f9c5b5e3f5ae210aa693112191998b68bdddff77
ductran67 <Josephductran67@gmail.com>
2022-08-29 17:18:38 -0700

    For deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// const path = require('path');
+const path = require('path');
 const express = require("express");
 const colors = require('colors');
 const routes = require("./routes");
@@ -14,5 +14,18 @@ server.use(express.urlencoded({ extended: true }));
 server.use(errorHandler);
 server.use(routes);
 server.use('/images', express.static('images'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  server.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  server.get('/', (req, res) => res.send('Please set to production'));
+}
 
 module.exports = server;
