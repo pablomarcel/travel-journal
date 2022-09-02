f22f36ad1a447633317a1efe3aefe565446cb9e0
Ryan Ku <rjhk@uw.edu>
2022-09-02 14:01:22 -0700

    added conditional for mock tests in server.js

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,9 @@ const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 
-
-connectDB();
+if (process.env.NODE_ENV != 'test') {
+  connectDB();
+}
 
 const server = express();
 server.use(express.json());
@@ -18,17 +19,17 @@ server.use('/images', express.static('images'));
 
 
 // Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  server.use(express.static(path.join(__dirname, '../frontend/build')));
+// if (process.env.NODE_ENV === 'production') {
+  // server.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  server.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
-} else {
-  server.get('/', (req, res) => res.send('Please set to production'));
-}
+  // server.get('*', (req, res) =>
+  //   res.sendFile(
+  //     path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+  //   )
+  // );
+// } else {
+//   server.get('/', (req, res) => res.send('Please set to production'));
+// }
 
 module.exports = server;
 
