7aa7a6908b1fc60cc542e25fe5a162fe6b58e5ec
Ryan Ku <rjhk@uw.edu>
2022-09-11 01:29:45 -0700

    tests and auth tests complete

---
+++ backend/tests/users.test.js
@@ -0,0 +1,131 @@
+const request = require("supertest");
+const server = require("../server");
+
+jest.mock("../dataInterface/users");
+const userData = require("../dataInterface/users");
+
+jest.mock('../middleware/authMiddleware');
+const authMock = require('../middleware/authMiddleware');
+
+describe("/users routes", () => {
+	beforeEach(() => {
+		jest.clearAllMocks()
+	});
+
+	describe("POST /users/login", () =>{
+		it("should return an array of posts on success", async () => {
+			authMock.protect.mockImplementation(function(req, res, next) {
+        req.user = "630ea30184c9d5dd0b5e50b7";
+        return next();
+      })
+
+			userData.findByCredentials.mockResolvedValue([{
+				_id: "630ea30184c9d5dd0b5e50b7",
+      	email: "example email"
+			}]);
+
+      const res = await request(server).post("/api/users/login");
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.error).not.toBeDefined();
+		});
+
+		it("should return an error message", async () => {
+			authMock.protect.mockImplementation(function(req, res, next) {
+        req.user = "630ea30184c9d5dd0b5e50b7";
+        return next();
+      })
+
+			userData.findByCredentials.mockResolvedValue({
+				error: "Password is incorrect!"
+			});
+
+      const res = await request(server).post("/api/users/login");
+
+      expect(res.statusCode).toEqual(400);
+      expect(res.body.error).toBeDefined();
+		});
+	});
+
+	// describe("GET /users/register", () =>{
+	// 	it("should return an array of posts on success", async () => {
+	// 		authMock.protect.mockImplementation(function(req, res, next) {
+  //       req.user = "630ea30184c9d5dd0b5e50b7";
+  //       return next();
+  //     })
+
+	// 		userData.findByCredentials.mockResolvedValue([{
+	// 			_id: "630ea30184c9d5dd0b5e50b7",
+  //     	email: "example email"
+	// 		}]);
+
+  //     const res = await request(server).get("/api/users/login");
+
+  //     expect(res.statusCode).toEqual(200);
+  //     expect(res.body.error).not.toBeDefined();
+	// 	});
+	// 	it("should return an error message", async () => {
+	// 		authMock.protect.mockImplementation(function(req, res, next) {
+  //       req.user = "630ea30184c9d5dd0b5e50b7";
+  //       return next();
+  //     })
+
+	// 		userData.findByCredentials.mockResolvedValue({
+	// 			error: "Password is incorrect!"
+	// 		});
+
+  //     const res = await request(server).get("/api/users/login");
+
+  //     expect(res.statusCode).toEqual(400);
+  //     expect(res.body.error).toBeDefined();
+	// 	});
+	// });
+
+	// describe("GET /users/:id", () =>{
+	// 	it("should return an array of posts on success", async () => {
+	// 		authMock.protect.mockImplementation(function(req, res, next) {
+  //       req.user = "630ea30184c9d5dd0b5e50b7";
+  //       return next();
+  //     })
+
+	// 		userData.findByCredentials.mockResolvedValue([{
+	// 			_id: "630ea30184c9d5dd0b5e50b7",
+  //     	email: "example email"
+	// 		}]);
+
+  //     const res = await request(server).get("/api/users/login");
+
+  //     expect(res.statusCode).toEqual(200);
+  //     expect(res.body.error).not.toBeDefined();
+	// 	});
+	// 	it("should return an error message", async () => {
+	// 		authMock.protect.mockImplementation(function(req, res, next) {
+  //       req.user = "630ea30184c9d5dd0b5e50b7";
+  //       return next();
+  //     })
+
+	// 		userData.findByCredentials.mockResolvedValue({
+	// 			error: "Password is incorrect!"
+	// 		});
+
+  //     const res = await request(server).get("/api/users/login");
+
+  //     expect(res.statusCode).toEqual(400);
+  //     expect(res.body.error).toBeDefined();
+	// 	});
+	// });
+
+	describe("GET /me", () => {
+		it("should return id of deleted post on success", async () => {
+		
+			authMock.protect.mockImplementation(function(req, res, next) {
+				req.user = "630ea30184c9d5dd0b5e50b7";
+				return next();
+			})
+
+			const res = await request(server).get("/api/users/me");
+
+			expect(res.statusCode).toEqual(200);
+		});
+	});
+})
